5ca5cfaa6463c5efa6f1be82e33002f6a65d15d7
Lilya Parilyak <lip@meshtech.no>
2020-04-26 18:52:51 +0300

    Switch Views

---
+++ SegmentedCtrlSwitchView/SimpleVC/SimpleVC2.swift
@@ -0,0 +1,30 @@
+//
+//  SimpleVC2.swift
+//  SegmentedCtrlSwitchView
+//
+//  Created by Lilya on 26.04.2020.
+//  Copyright © 2020 Lilya. All rights reserved.
+//
+
+import UIKit
+
+class SimpleVC2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SegmentedCtrlSwitchView/ViewController.swift
+++ SegmentedCtrlSwitchView/ViewController.swift
@@ -12,12 +12,30 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var viewContainer: UIView!
     
+    var simpleView1: UIView!
+    var simpleView2: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        simpleView1 = SimpleVC1().view
+        simpleView2 = SimpleVC2().view
+        viewContainer.addSubview(simpleView2)
+        viewContainer.addSubview(simpleView1)
     }
 
     @IBAction func switchViewAction(_ sender: UISegmentedControl) {
+        
+        switch sender.selectedSegmentIndex {
+        case 0:
+            viewContainer.bringSubviewToFront(simpleView1)
+            break
+        case 1:
+            viewContainer.bringSubviewToFront(simpleView2)
+            break
+        default:
+            break
+        }
     }
     
 }
