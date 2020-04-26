92610cffb6c971f8461bd245f9a7b218ecc3c687
Lilya Parilyak <lip@meshtech.no>
2020-04-26 18:36:59 +0300

    Added segmented control and a view container

--- SegmentedCtrlSwitchView/ViewController.swift
+++ SegmentedCtrlSwitchView/ViewController.swift
@@ -9,12 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var viewContainer: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func switchViewAction(_ sender: UISegmentedControl) {
+    }
+    
 }
 
