c4f9f488c65f760b53aa585321c0a210406ba08e
Lilya Parilyak <lip@meshtech.no>
2020-04-26 18:31:17 +0300

    Initial Commit

---
+++ SegmentedCtrlSwitchView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SegmentedCtrlSwitchView
+//
+//  Created by Lilya on 26.04.2020.
+//  Copyright © 2020 Lilya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
