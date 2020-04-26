080fa9d3e8ce1cfd8728d48ba33bed4e90559559
Lilya Parilyak <lip@meshtech.no>
2020-04-26 21:46:07 +0300

    Added iCarousel

--- SegmentedCtrlSwitchView/SimpleVC/SimpleVC2.swift
+++ SegmentedCtrlSwitchView/SimpleVC/SimpleVC2.swift
@@ -9,22 +9,44 @@
 import UIKit
 
 class SimpleVC2: UIViewController {
+    
+    @IBOutlet weak var iCarouselView: iCarousel!
+    
+    var imgArr = [ UIImage(named: "coding"),
+                   UIImage(named: "sports"),
+                   UIImage(named: "ballet"),
+                   UIImage(named: "litclub"),
+                   UIImage(named: "arts")
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        iCarouselView.type = .timeMachine
+        iCarouselView.contentMode = .scaleAspectFill
+        iCarouselView.isPagingEnabled = true
     }
 
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension SimpleVC2: iCarouselDelegate, iCarouselDataSource {
+    func numberOfItems(in carousel: iCarousel) -> Int {
+        return imgArr.count
     }
-    */
-
+    
+    func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView {
+        
+        var imageView: UIImageView!
+        if view == nil {
+            imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width - 40, height: 250))
+            imageView.contentMode = .scaleAspectFit
+        } else {
+            imageView = view as? UIImageView
+        }
+        
+        imageView.image = imgArr[index]
+        return imageView
+    }
+    
+    
 }
