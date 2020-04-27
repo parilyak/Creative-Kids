4bb8149f3112fbeb4c0526fe47e9186436d79082
Lilya Parilyak <lip@meshtech.no>
2020-04-27 09:40:00 +0300

    Added iCarousel

--- SegmentedCtrlSwitchView/SimpleVC/SimpleVC2.swift
+++ SegmentedCtrlSwitchView/SimpleVC/SimpleVC2.swift
@@ -1,52 +1,52 @@
+////
+////  SimpleVC2.swift
+////  SegmentedCtrlSwitchView
+////
+////  Created by Lilya on 26.04.2020.
+////  Copyright © 2020 Lilya. All rights reserved.
+////
 //
-//  SimpleVC2.swift
-//  SegmentedCtrlSwitchView
+//import UIKit
 //
-//  Created by Lilya on 26.04.2020.
-//  Copyright © 2020 Lilya. All rights reserved.
+//class SimpleVC2: UIViewController {
+//    
+//    @IBOutlet weak var iCarouselView: iCarousel!
+//    
+//    var imgArr = [ UIImage(named: "coding"),
+//                   UIImage(named: "sports"),
+//                   UIImage(named: "ballet"),
+//                   UIImage(named: "litclub"),
+//                   UIImage(named: "arts")
+//    ]
 //
-
-import UIKit
-
-class SimpleVC2: UIViewController {
-    
-    @IBOutlet weak var iCarouselView: iCarousel!
-    
-    var imgArr = [ UIImage(named: "coding"),
-                   UIImage(named: "sports"),
-                   UIImage(named: "ballet"),
-                   UIImage(named: "litclub"),
-                   UIImage(named: "arts")
-    ]
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        iCarouselView.type = .timeMachine
-        iCarouselView.contentMode = .scaleAspectFill
-        iCarouselView.isPagingEnabled = true
-    }
-
-}
-
-extension SimpleVC2: iCarouselDelegate, iCarouselDataSource {
-    func numberOfItems(in carousel: iCarousel) -> Int {
-        return imgArr.count
-    }
-    
-    func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView {
-        
-        var imageView: UIImageView!
-        if view == nil {
-            imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width - 40, height: 250))
-            imageView.contentMode = .scaleAspectFit
-        } else {
-            imageView = view as? UIImageView
-        }
-        
-        imageView.image = imgArr[index]
-        return imageView
-    }
-    
-    
-}
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        iCarouselView.type = .timeMachine
+//        iCarouselView.contentMode = .scaleAspectFill
+//        iCarouselView.isPagingEnabled = true
+//    }
+//
+//}
+//
+//extension SimpleVC2: iCarouselDelegate, iCarouselDataSource {
+//    func numberOfItems(in carousel: iCarousel) -> Int {
+//        return imgArr.count
+//    }
+//    
+//    func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView {
+//        
+//        var imageView: UIImageView!
+//        if view == nil {
+//            imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width - 40, height: 250))
+//            imageView.contentMode = .scaleAspectFit
+//        } else {
+//            imageView = view as? UIImageView
+//        }
+//        
+//        imageView.image = imgArr[index]
+//        return imageView
+//    }
+//    
+//    
+//}
--- SegmentedCtrlSwitchView/ViewController.swift
+++ SegmentedCtrlSwitchView/ViewController.swift
@@ -11,26 +11,32 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var viewContainer: UIView!
-   
-    var views:  [UIView]!
+    
+    @IBOutlet weak var container: UIView!
+    
+    var viewNames:  [String] = ["SimpleVC1", "GalleryVC", "SimpleVC3"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        views = [UIView]()
-        views.append(SimpleVC1().view)
-        views.append(SimpleVC2().view)
-        views.append(SimpleVC3().view)
         
-        for v in views {
-            viewContainer.addSubview(v)
-        }
-        
-        viewContainer.bringSubviewToFront(views[0])
+        add(storyboardName: viewNames[1])
     }
-
+    
     @IBAction func switchViewAction(_ sender: UISegmentedControl) {
-        self.viewContainer.bringSubviewToFront(views[sender.selectedSegmentIndex])
+        
+        print(sender.selectedSegmentIndex)
+        add(storyboardName: viewNames[sender.selectedSegmentIndex])
+    }
+    
+    func add(storyboardName: String) {
+        container.subviews.forEach {sub in
+            sub.removeFromSuperview()
+        }
+        let controller = storyboard!.instantiateViewController(withIdentifier: storyboardName)
+        addChild(controller)
+        controller.view.frame = container.frame
+        container.addSubview(controller.view)
+        controller.didMove(toParent: self)
     }
     
 }
