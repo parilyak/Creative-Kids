72d351fae4d057f7fdec590a0726d7680a22f2e3
Lilya Parilyak <lip@meshtech.no>
2020-04-27 10:19:48 +0300

    Added app icon and custom cell files

---
+++ SegmentedCtrlSwitchView/SwitchedViews/CustomCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomCell.swift
+//  SegmentedCtrlSwitchView
+//
+//  Created by Lilya on 27.04.2020.
+//  Copyright © 2020 Lilya. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
